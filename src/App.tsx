e74cc21cf91a6baf23f9f2941c38fed52291bbca
noreplyrestriction1-art <no.reply.restriction1@gmail.com>
2026-08-31 00:11:16 +0700

    Fix white screen by updating App.tsx and index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GoogleSignIn from './GoogleSignIn';
-import { Dashboard } from './Dashboard';
 
-export function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GoogleSignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<GoogleSignIn />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  return <GoogleSignIn />;
 }
-
-export default App;
