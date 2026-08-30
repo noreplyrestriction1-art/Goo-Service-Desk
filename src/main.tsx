75c25f381d7a644e8e5e1fb6b7512098e0638533
noreplyrestriction1-art <no.reply.restriction1@gmail.com>
2026-08-31 00:20:00 +0700

    Add mobile error alert popup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
+
+window.onerror = function (message, source, lineno, colno, error) {
+  alert("Error: " + message + " at line " + lineno);
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
